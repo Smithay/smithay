38708c4eadd5377adf012ac076d29b94e4a3fe0f
Victor Berger <victor.berger@m4x.org>
2017-03-11 09:15:17 +0100

    travis: Add clippy

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 #![warn(missing_docs)]
 
+#![cfg_attr(feature = "clippy", feature(plugin))]
+#![cfg_attr(feature = "clippy", plugin(clippy))]
+
 #[macro_use]
 extern crate wayland_server;
 extern crate nix;
