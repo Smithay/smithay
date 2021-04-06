4ed7823db84bf530b6c62d3153ae1b3f4d0cb62e
Victor Brekenfeld <github@drakulix.de>
2021-04-07 01:04:21 +0200

    Remove the generic "graphics" module.
    - Cursor is not necessary anymore as rendering hardware vs software
      cursors just depends on "where" you render the cursor.
    - Format is replaced by drm-fourcc and more related to buffers and
      therefor the allocator module anyway.
    - GL/Glium are replaced by the renderer module.
    - Software was unused and very small anyway and can be implemented
      with the new renderer api.
    - SwapBuffersError is (for now) just moved into the top-level backend module.

--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -14,8 +14,13 @@
 //! - winit
 //! - libinput
 
-pub mod graphics;
+// TODO TEMPORARY
+#![allow(missing_docs)]
+
+//pub mod graphics;
 pub mod input;
+pub mod allocator;
+pub mod renderer;
 
 #[cfg(feature = "backend_drm")]
 pub mod drm;
@@ -27,5 +32,38 @@ pub mod libinput;
 pub mod session;
 #[cfg(feature = "backend_udev")]
 pub mod udev;
+
 #[cfg(feature = "backend_winit")]
 pub mod winit;
+
+/// Error that can happen when swapping buffers.
+#[derive(Debug, thiserror::Error)]
+pub enum SwapBuffersError {
+    /// The buffers have already been swapped.
+    ///
+    /// This error can be returned when `swap_buffers` has been called multiple times
+    /// without any modification in between.
+    #[error("Buffers are already swapped, swap_buffers was called too many times")]
+    AlreadySwapped,
+    /// The corresponding context has been lost and needs to be recreated.
+    ///
+    /// All the objects associated to it (textures, buffers, programs, etc.)
+    /// need to be recreated from scratch. Underlying resources like native surfaces
+    /// might also need to be recreated.
+    ///
+    /// Operations will have no effect. Functions that read textures, buffers, etc.
+    /// will return uninitialized data instead.
+    #[error("The context has been lost, it needs to be recreated: {0}")]
+    ContextLost(Box<dyn std::error::Error>),
+    /// A temporary condition caused to rendering to fail.
+    ///
+    /// Depending on the underlying error this *might* require fixing internal state of the rendering backend,
+    /// but failures mapped to `TemporaryFailure` are always recoverable without re-creating the entire stack,
+    /// as is represented by `ContextLost`.
+    ///
+    /// Proceed after investigating the source to reschedule another full rendering step or just this page_flip at a later time.
+    /// If the root cause cannot be discovered and subsequent renderings also fail, it is advised to fallback to
+    /// recreation.
+    #[error("A temporary condition caused the page flip to fail: {0}")]
+    TemporaryFailure(Box<dyn std::error::Error>),
+}
