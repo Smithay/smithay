389871ed5fde1ccaef97973a3ded8bd4572d82bb
Victor Berger <victor.berger@m4x.org>
2021-07-01 22:23:13 +0200

    wayland.shell.legacy: review docs & API

--- src/wayland/shell/legacy/wl_handlers.rs
+++ src/wayland/shell/legacy/wl_handlers.rs
@@ -7,7 +7,7 @@ use std::{
 
 use wayland_server::{
     protocol::{wl_shell, wl_shell_surface, wl_surface},
-    Filter, Main,
+    DispatchData, Filter, Main,
 };
 
 static WL_SHELL_SURFACE_ROLE: &str = "wl_shell_surface";
@@ -22,9 +22,9 @@ pub(crate) fn implement_shell<Impl>(
     implementation: Rc<RefCell<Impl>>,
     state: Arc<Mutex<ShellState>>,
 ) where
-    Impl: FnMut(ShellRequest) + 'static,
+    Impl: FnMut(ShellRequest, DispatchData<'_>) + 'static,
 {
-    shell.quick_assign(move |shell, req, _data| {
+    shell.quick_assign(move |shell, req, data| {
         let (id, surface) = match req {
             wl_shell::Request::GetShellSurface { id, surface } => (id, surface),
             _ => unreachable!(),
@@ -52,9 +52,12 @@ pub(crate) fn implement_shell<Impl>(
             .known_surfaces
             .push(make_handle(&shell_surface));
         let mut imp = implementation.borrow_mut();
-        (&mut *imp)(ShellRequest::NewShellSurface {
-            surface: make_handle(&shell_surface),
-        });
+        (&mut *imp)(
+            ShellRequest::NewShellSurface {
+                surface: make_handle(&shell_surface),
+            },
+            data,
+        );
     });
 }
 
@@ -82,10 +85,10 @@ fn implement_shell_surface<Impl>(
     state: Arc<Mutex<ShellState>>,
 ) -> wl_shell_surface::WlShellSurface
 where
-    Impl: FnMut(ShellRequest) + 'static,
+    Impl: FnMut(ShellRequest, DispatchData<'_>) + 'static,
 {
     use self::wl_shell_surface::Request;
-    shell_surface.quick_assign(move |shell_surface, req, _data| {
+    shell_surface.quick_assign(move |shell_surface, req, dispatch_data| {
         let data = shell_surface
             .as_ref()
             .user_data()
@@ -111,52 +114,70 @@ where
                 })
                 .unwrap();
                 if valid {
-                    (&mut *user_impl)(ShellRequest::Pong {
-                        surface: make_handle(&shell_surface),
-                    });
+                    (&mut *user_impl)(
+                        ShellRequest::Pong {
+                            surface: make_handle(&shell_surface),
+                        },
+                        dispatch_data,
+                    );
                 }
             }
             Request::Move { seat, serial } => {
                 let serial = Serial::from(serial);
-                (&mut *user_impl)(ShellRequest::Move {
-                    surface: make_handle(&shell_surface),
-                    serial,
-                    seat,
-                })
+                (&mut *user_impl)(
+                    ShellRequest::Move {
+                        surface: make_handle(&shell_surface),
+                        serial,
+                        seat,
+                    },
+                    dispatch_data,
+                )
             }
             Request::Resize { seat, serial, edges } => {
                 let serial = Serial::from(serial);
-                (&mut *user_impl)(ShellRequest::Resize {
-                    surface: make_handle(&shell_surface),
-                    serial,
-                    seat,
-                    edges,
-                })
+                (&mut *user_impl)(
+                    ShellRequest::Resize {
+                        surface: make_handle(&shell_surface),
+                        serial,
+                        seat,
+                        edges,
+                    },
+                    dispatch_data,
+                )
             }
-            Request::SetToplevel => (&mut *user_impl)(ShellRequest::SetKind {
-                surface: make_handle(&shell_surface),
-                kind: ShellSurfaceKind::Toplevel,
-            }),
-            Request::SetTransient { parent, x, y, flags } => (&mut *user_impl)(ShellRequest::SetKind {
-                surface: make_handle(&shell_surface),
-                kind: ShellSurfaceKind::Transient {
-                    parent,
-                    location: (x, y),
-                    inactive: flags.contains(wl_shell_surface::Transient::Inactive),
+            Request::SetToplevel => (&mut *user_impl)(
+                ShellRequest::SetKind {
+                    surface: make_handle(&shell_surface),
+                    kind: ShellSurfaceKind::Toplevel,
                 },
-            }),
+                dispatch_data,
+            ),
+            Request::SetTransient { parent, x, y, flags } => (&mut *user_impl)(
+                ShellRequest::SetKind {
+                    surface: make_handle(&shell_surface),
+                    kind: ShellSurfaceKind::Transient {
+                        parent,
+                        location: (x, y),
+                        inactive: flags.contains(wl_shell_surface::Transient::Inactive),
+                    },
+                },
+                dispatch_data,
+            ),
             Request::SetFullscreen {
                 method,
                 framerate,
                 output,
-            } => (&mut *user_impl)(ShellRequest::SetKind {
-                surface: make_handle(&shell_surface),
-                kind: ShellSurfaceKind::Fullscreen {
-                    method,
-                    framerate,
-                    output,
+            } => (&mut *user_impl)(
+                ShellRequest::SetKind {
+                    surface: make_handle(&shell_surface),
+                    kind: ShellSurfaceKind::Fullscreen {
+                        method,
+                        framerate,
+                        output,
+                    },
                 },
-            }),
+                dispatch_data,
+            ),
             Request::SetPopup {
                 seat,
                 serial,
@@ -166,21 +187,27 @@ where
                 flags,
             } => {
                 let serial = Serial::from(serial);
-                (&mut *user_impl)(ShellRequest::SetKind {
-                    surface: make_handle(&shell_surface),
-                    kind: ShellSurfaceKind::Popup {
-                        parent,
-                        serial,
-                        seat,
-                        location: (x, y),
-                        inactive: flags.contains(wl_shell_surface::Transient::Inactive),
+                (&mut *user_impl)(
+                    ShellRequest::SetKind {
+                        surface: make_handle(&shell_surface),
+                        kind: ShellSurfaceKind::Popup {
+                            parent,
+                            serial,
+                            seat,
+                            location: (x, y),
+                            inactive: flags.contains(wl_shell_surface::Transient::Inactive),
+                        },
                     },
-                })
+                    dispatch_data,
+                )
             }
-            Request::SetMaximized { output } => (&mut *user_impl)(ShellRequest::SetKind {
-                surface: make_handle(&shell_surface),
-                kind: ShellSurfaceKind::Maximized { output },
-            }),
+            Request::SetMaximized { output } => (&mut *user_impl)(
+                ShellRequest::SetKind {
+                    surface: make_handle(&shell_surface),
+                    kind: ShellSurfaceKind::Maximized { output },
+                },
+                dispatch_data,
+            ),
             Request::SetTitle { title } => {
                 compositor::with_states(&data.surface, |states| {
                     let mut guard = states
