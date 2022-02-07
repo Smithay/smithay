49e1328d8e93f6e6ba818486b6d0603c50cc3a3c
Victoria Brekenfeld <github@drakulix.de>
2022-02-07 18:51:50 +0100

    ci: Update wlcs

--- compile_wlcs.sh
+++ compile_wlcs.sh
@@ -7,7 +7,7 @@ else
     git clone https://github.com/MirServer/wlcs.git
     cd wlcs
     # checkout a specific revision
-    git reset --hard dcacf09b60f1c5caf103a58046c547df6bb0b85b
+    git reset --hard dbee179e91a140d0725e15ba60ea12f6c89d0904
     cmake -DWLCS_BUILD_ASAN=False -DWLCS_BUILD_TSAN=False -DWLCS_BUILD_UBSAN=False .
     make
 fi
