3e2fe47703ec8881f42b76e9ec8a46cdd05f5ad2
Christian Meissl <meissl.christian@gmail.com>
2022-08-10 16:48:23 +0200

    wip: render elements

---
+++ src/backend/drm/renderer.rs
@@ -0,0 +1,55 @@
+use crate::backend::renderer::{
+    buffer_type,
+    output::{
+        element::{RenderElement, UnderlyingStorage},
+        OutputRender,
+    },
+    BufferType, ImportAll, Renderer, Texture,
+};
+
+/// Specialized render for drm output which uses planes
+#[derive(Debug)]
+pub struct DrmOutputRender {
+    output_render: OutputRender,
+}
+
+impl DrmOutputRender {
+    /// Render the output
+    pub fn render_output<R, E>(&mut self, renderer: &mut R, elements: &[E], log: &slog::Logger)
+    where
+        E: RenderElement<R>,
+        R: Renderer + ImportAll + std::fmt::Debug,
+        <R as Renderer>::TextureId: Texture + std::fmt::Debug + 'static,
+    {
+        for element in elements {
+            if let Some(underlying_storage) = element.underlying_storage(renderer) {
+                match underlying_storage {
+                    UnderlyingStorage::Wayland(buffer) => {
+                        let buffer_type = buffer_type(&buffer);
+
+                        let buffer_supports_direct_scan_out = if let Some(buffer_type) = buffer_type {
+                            matches!(buffer_type, BufferType::Dma)
+                        } else {
+                            false
+                        };
+
+                        if buffer_supports_direct_scan_out {
+                            // Try to assign the element to a plane
+                            todo!()
+                        } else {
+                            // No direct-scan out possible, needs to be rendered on the primary plane
+                        }
+                    }
+                    UnderlyingStorage::External(_) => {
+                        // No direct-scan out possible, needs to be rendered on the primary plane
+                    }
+                }
+            } else {
+                // No direct-scan out possible, needs to be rendered on the primary plane
+            }
+        }
+
+        // Draw the remaining elements on the primary plane
+        self.output_render.render_output(renderer, 0, elements, log);
+    }
+}
---
+++ src/backend/renderer/output/element/texture.rs
@@ -0,0 +1,92 @@
+use crate::{
+    backend::renderer::{Frame, Renderer, Texture},
+    utils::{Physical, Point, Rectangle, Scale, Transform},
+};
+
+use super::{Id, RenderElement, UnderlyingStorage};
+
+/// A single texture render element
+#[derive(Debug)]
+pub struct TextureRenderElement<R>
+where
+    R: Renderer,
+    <R as Renderer>::TextureId: Texture,
+{
+    location: Point<i32, Physical>,
+    id: Id,
+    texture: R::TextureId,
+}
+
+impl<R> TextureRenderElement<R>
+where
+    R: Renderer,
+    <R as Renderer>::TextureId: Texture,
+{
+    /// Create a texture render element from an existing texture
+    pub fn from_texture(
+        location: impl Into<Point<i32, Physical>>,
+        id: Id,
+        texture: R::TextureId,
+    ) -> Self {
+        Self {
+            location: location.into(),
+            id,
+            texture,
+        }
+    }
+}
+
+impl<R> RenderElement<R> for TextureRenderElement<R>
+where
+    R: Renderer,
+    <R as Renderer>::TextureId: Texture,
+{
+    fn id(&self) -> &Id {
+        &self.id
+    }
+
+    fn current_commit(&self) -> usize {
+        1
+    }
+
+    fn location(&self, _scale: Scale<f64>) -> Point<i32, Physical> {
+        self.location
+    }
+
+    fn geometry(&self, scale: Scale<f64>) -> Rectangle<i32, Physical> {
+        Rectangle::from_loc_and_size(self.location, (64, 64))
+    }
+
+    fn damage_since(&self, scale: Scale<f64>, commit: Option<usize>) -> Vec<Rectangle<i32, Physical>> {
+        vec![]
+    }
+
+    fn opaque_regions(&self, scale: Scale<f64>) -> Vec<Rectangle<i32, Physical>> {
+        vec![]
+    }
+
+    fn underlying_storage(&self, renderer: &R) -> Option<UnderlyingStorage<'_, R>> {
+        todo!()
+    }
+
+    fn draw(
+        &self,
+        renderer: &mut R,
+        frame: &mut <R as Renderer>::Frame,
+        scale: Scale<f64>,
+        damage: &[Rectangle<i32, Physical>],
+        log: &slog::Logger,
+    ) {
+        frame
+            .render_texture_at(
+                &self.texture,
+                self.location,
+                1,
+                scale,
+                Transform::Normal,
+                damage,
+                1.0,
+            )
+            .expect("failed to render")
+    }
+}
