8c11a51af795472af3710d3ced7c3b5118401f34
Christian Meissl <meissl.christian@gmail.com>
2022-08-17 18:40:01 +0200

    fix macro for specific renderer

--- src/desktop/mod.rs
+++ src/desktop/mod.rs
@@ -51,11 +51,11 @@
 
 pub(crate) mod layer;
 mod popup;
-pub mod space;
+//pub mod space;
 pub mod utils;
 mod window;
 
 pub use self::layer::{layer_map_for_output, LayerMap, LayerSurface};
 pub use self::popup::*;
-pub use self::space::Space;
+//pub use self::space::Space;
 pub use self::window::*;
