6059220b04bc80e8c7bcac67ff3969c783fa7784
TornaxO7 <tornax@pm.me>
2023-03-13 18:26:04 +0100

    forbid `unsafe_op_in_unsafe_fn` in library

--- src/backend/allocator/vulkan/mod.rs
+++ src/backend/allocator/vulkan/mod.rs
@@ -16,8 +16,6 @@
 //! To get the required extensions a device must support to use the Vulkan allocator, use
 //! [`VulkanAllocator::required_extensions`].
 
-#![deny(unsafe_op_in_unsafe_fn)]
-
 pub mod format;
 
 use std::{
--- src/backend/vulkan/mod.rs
+++ src/backend/vulkan/mod.rs
@@ -63,7 +63,6 @@
 //! instance and have the same physical device handle.
 
 #![warn(missing_debug_implementations)]
-#![deny(unsafe_op_in_unsafe_fn)]
 
 use std::{
     env::{self, VarError},
