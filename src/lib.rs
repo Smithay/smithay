09c0027305c4dbcbe1dc5c21f9a6bbd5f281d4e9
TornaxO7 <tornax@pm.me>
2023-03-15 17:27:04 +0100

    adding line to forbid unsafe operation without unsafe block

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 #![warn(missing_docs, missing_debug_implementations, rust_2018_idioms)]
 // Allow acronyms like EGL
 #![allow(clippy::upper_case_acronyms)]
+#![forbid(unsafe_op_in_unsafe_fn)]
 
 //! # Smithay: the Wayland compositor smithy
 //!
