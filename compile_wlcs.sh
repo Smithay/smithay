637d79a6026dae1ed7006b157ce40f714b4ee013
Ben Widawsky <ben@bwidawsk.net>
2023-06-21 09:58:05 -0700

    compile_wlcs: Make shellcheck compliant

--- compile_wlcs.sh
+++ compile_wlcs.sh
@@ -5,7 +5,7 @@ if [ -f "./wlcs/wlcs" ]; then
 else
     echo "Compiling WLCS."
     git clone https://github.com/MirServer/wlcs.git
-    cd wlcs
+    cd wlcs || exit
     # checkout a specific revision
     git reset --hard 34e4804574324fa9f09fe85c19037bcc1444c465
     cmake -DWLCS_BUILD_ASAN=False -DWLCS_BUILD_TSAN=False -DWLCS_BUILD_UBSAN=False .
