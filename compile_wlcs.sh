82349491c30c2f8850ab433eeaab876af0e45054
Ben Widawsky <ben@bwidawsk.net>
2023-06-21 09:54:09 -0700

    compile_wlcs: Move SHA to variable

--- compile_wlcs.sh
+++ compile_wlcs.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+WLCS_SHA=34e4804574324fa9f09fe85c19037bcc1444c465
+
 if [ -f "./wlcs/wlcs" ]; then
     echo "Using cached WLCS."
 else
@@ -7,7 +9,7 @@ else
     git clone https://github.com/MirServer/wlcs.git
     cd wlcs || exit
     # checkout a specific revision
-    git reset --hard 34e4804574324fa9f09fe85c19037bcc1444c465
+    git reset --hard "${WLCS_SHA}"
     cmake -DWLCS_BUILD_ASAN=False -DWLCS_BUILD_TSAN=False -DWLCS_BUILD_UBSAN=False .
     make
 fi
