0b672a622cc133b83a8a0d9e05a88a0de1778ba7
Christian Meissl <meissl.christian@gmail.com>
2023-11-25 19:51:18 +0100

    backend: export dumb buffer as rdwr
    this allows the resulting dmabuf to be mapped
    read/write for rendering

--- src/backend/allocator/dumb.rs
+++ src/backend/allocator/dumb.rs
@@ -96,7 +96,9 @@ impl AsDmabuf for DumbBuffer {
 
     #[profiling::function]
     fn export(&self) -> Result<Dmabuf, Self::Error> {
-        let fd = self.fd.buffer_to_prime_fd(self.handle.handle(), 0)?;
+        let fd = self
+            .fd
+            .buffer_to_prime_fd(self.handle.handle(), drm::CLOEXEC | drm::RDWR)?;
         let mut builder = Dmabuf::builder(
             self.size(),
             self.format.code,
