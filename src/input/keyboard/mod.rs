Smithay/smithay

commit 629975f71470930b9634e814e405a869915588c1

Fix deadlocks when updating xkb state
change_keymap() needs a lock on self.arc.internal.

src/input/keyboard/mod.rs
@@ -607,7 +607,9 @@ impl<D: SeatHandler + 'static> KeyboardHandle<D> {
         true
     }
 
-    fn update_xkb_state(&self, data: &mut D, internal: &mut KbdInternal<D>, keymap: xkb::Keymap) {
+    fn update_xkb_state(&self, data: &mut D, keymap: xkb::Keymap) {
+        let mut internal = self.arc.internal.lock().unwrap();
+
         let mut state = xkb::State::new(&keymap);
         for key in &internal.pressed_keys {
             state.update_key((key + 8).into(), xkb::KeyDirection::Down);
@@ -623,6 +625,9 @@ impl<D: SeatHandler + 'static> KeyboardHandle<D> {
             focus.modifiers(&seat, data, mods, SERIAL_COUNTER.next_serial());
         };
 
+        // Release the lock.
+        drop(internal);
+
         #[cfg(feature = "wayland_frontend")]
         self.change_keymap(data, &keymap);
     }
@@ -631,7 +636,6 @@ impl<D: SeatHandler + 'static> KeyboardHandle<D> {
     ///
     /// The input is a keymap in XKB_KEYMAP_FORMAT_TEXT_V1 format.
     pub fn set_keymap_from_string(&self, data: &mut D, keymap: String) -> Result<(), Error> {
-        let mut internal = self.arc.internal.lock().unwrap();
         // Construct the Keymap internally instead of accepting one as input
         // because libxkbcommon is not thread-safe.
         let keymap = xkb::Keymap::new_from_string(
@@ -644,18 +648,19 @@ impl<D: SeatHandler + 'static> KeyboardHandle<D> {
             debug!("Loading keymap from string failed");
             Error::BadKeymap
         })?;
-        self.update_xkb_state(data, &mut internal, keymap);
+        self.update_xkb_state(data, keymap);
         Ok(())
     }
 
     /// Change the [`XkbConfig`] used by the keyboard.
     pub fn set_xkb_config(&self, data: &mut D, xkb_config: XkbConfig<'_>) -> Result<(), Error> {
-        let mut internal = self.arc.internal.lock().unwrap();
-        let keymap = xkb_config.compile_keymap(&internal.context).map_err(|_| {
-            debug!("Loading keymap from XkbConfig failed");
-            Error::BadKeymap
-        })?;
-        self.update_xkb_state(data, &mut internal, keymap);
+        let keymap = xkb_config
+            .compile_keymap(&self.arc.internal.lock().unwrap().context)
+            .map_err(|_| {
+                debug!("Loading keymap from XkbConfig failed");
+                Error::BadKeymap
+            })?;
+        self.update_xkb_state(data, keymap);
         Ok(())
     }
 

commit c59b987b95c0762006914fb2fe8aa8ef299dff99

Fix deadlock in send_keymap when called form input
Pass the actually required types we need locks for instead of
ensuring that callers release the locks everywhere.

Fixes: 629975f714 (Fix deadlocks when updating xkb state)
Fixes: 5a0b08c804 (Add separate keymap for virtual keyboard)
Links: https://github.com/YaLTeR/niri/issues/103

src/input/keyboard/mod.rs
@@ -558,12 +558,17 @@ impl<D: SeatHandler + 'static> KeyboardHandle<D> {
 
     #[cfg(feature = "wayland_frontend")]
     #[instrument(parent = &self.arc.span, skip(self, data, keymap))]
-    pub(crate) fn change_keymap(&self, data: &mut D, keymap: &xkb::Keymap) {
+    pub(crate) fn change_keymap(
+        &self,
+        data: &mut D,
+        focus: &Option<&mut <D as SeatHandler>::KeyboardFocus>,
+        keymap: &xkb::Keymap,
+        mods: ModifiersState,
+    ) {
         let mut keymap_file = self.arc.keymap.lock().unwrap();
         keymap_file.change_keymap(keymap);
 
-        let mods = self.arc.internal.lock().unwrap().mods_state;
-        self.send_keymap(data, &keymap_file, mods);
+        self.send_keymap(data, focus, &keymap_file, mods);
     }
 
     /// Send a new wl_keyboard keymap, without updating the internal keymap.
@@ -571,7 +576,13 @@ impl<D: SeatHandler + 'static> KeyboardHandle<D> {
     /// Returns `true` if the keymap changed from the previous keymap.
     #[cfg(feature = "wayland_frontend")]
     #[instrument(parent = &self.arc.span, skip(self, data, keymap_file))]
-    pub(crate) fn send_keymap(&self, data: &mut D, keymap_file: &KeymapFile, mods: ModifiersState) -> bool {
+    pub(crate) fn send_keymap(
+        &self,
+        data: &mut D,
+        focus: &Option<&mut <D as SeatHandler>::KeyboardFocus>,
+        keymap_file: &KeymapFile,
+        mods: ModifiersState,
+    ) -> bool {
         use std::os::unix::io::AsFd;
         use tracing::warn;
         use wayland_server::{protocol::wl_keyboard::KeymapFormat, Resource};
@@ -599,8 +610,7 @@ impl<D: SeatHandler + 'static> KeyboardHandle<D> {
 
         // Send updated modifiers.
         let seat = self.get_seat(data);
-        let mut internal = self.arc.internal.lock().unwrap();
-        if let Some((focus, _)) = internal.focus.as_mut() {
+        if let Some(focus) = focus {
             focus.modifiers(&seat, data, mods, SERIAL_COUNTER.next_serial());
         }
 
@@ -616,20 +626,20 @@ impl<D: SeatHandler + 'static> KeyboardHandle<D> {
         }
 
         internal.mods_state.update_with(&state);
-        internal.state = state;
         internal.keymap = keymap.clone();
+        internal.state = state;
 
         let mods = internal.mods_state;
-        let seat = self.get_seat(data);
-        if let Some((focus, _)) = internal.focus.as_mut() {
+        let focus = internal.focus.as_mut().map(|(focus, _)| focus);
+
+        #[cfg(not(feature = "wayland_frontend"))]
+        if let Some(focus) = focus.as_ref() {
+            let seat = self.get_seat(data);
             focus.modifiers(&seat, data, mods, SERIAL_COUNTER.next_serial());
         };
 
-        // Release the lock.
-        drop(internal);
-
         #[cfg(feature = "wayland_frontend")]
-        self.change_keymap(data, &keymap);
+        self.change_keymap(data, &focus, &keymap, mods);
     }
 
     /// Change the [`Keymap`](xkb::Keymap) used by the keyboard.
@@ -996,7 +1006,7 @@ impl<'a, D: SeatHandler + 'static> KeyboardInnerHandle<'a, D> {
         if let Some(keyboard_handle) = self.seat.get_keyboard() {
             let keymap_file = keyboard_handle.arc.keymap.lock().unwrap();
             let mods = self.inner.mods_state;
-            keyboard_handle.send_keymap(data, &keymap_file, mods);
+            keyboard_handle.send_keymap(data, &Some(focus), &keymap_file, mods);
         }
 
         // key event must be sent before modifers event for libxkbcommon

src/wayland/virtual_keyboard/virtual_keyboard_handle.rs
@@ -111,11 +111,12 @@ where
 
                 // Ensure virtual keyboard's keymap is active.
                 let keyboard_handle = data.seat.get_keyboard().unwrap();
-                keyboard_handle.send_keymap(user_data, &vk_state.keymap, vk_state.mods);
+                let mut internal = keyboard_handle.arc.internal.lock().unwrap();
+                let focus = internal.focus.as_mut().map(|(focus, _)| focus);
+                keyboard_handle.send_keymap(user_data, &focus, &vk_state.keymap, vk_state.mods);
 
-                let internal = keyboard_handle.arc.internal.lock().unwrap();
-                if let Some(focus) = internal.focus.as_ref().and_then(|f| f.0.wl_surface()) {
-                    for_each_focused_kbds(&data.seat, &focus, |kbd| {
+                if let Some(wl_surface) = focus.and_then(|f| f.wl_surface()) {
+                    for_each_focused_kbds(&data.seat, &wl_surface, |kbd| {
                         // This should be wl_keyboard::KeyState, but the protocol does not state
                         // the parameter is an enum.
                         let key_state = if state == 1 {
@@ -152,13 +153,14 @@ where
 
                 // Ensure virtual keyboard's keymap is active.
                 let keyboard_handle = data.seat.get_keyboard().unwrap();
-                let keymap_changed = keyboard_handle.send_keymap(user_data, &state.keymap, state.mods);
+                let mut internal = keyboard_handle.arc.internal.lock().unwrap();
+                let focus = internal.focus.as_mut().map(|(focus, _)| focus);
+                let keymap_changed =
+                    keyboard_handle.send_keymap(user_data, &focus, &state.keymap, state.mods);
 
-                // Skip sending mods if keymap change already reported it.
+                // Report modifiers change to all keyboards.
                 if !keymap_changed {
-                    // Report modifiers change to all keyboards.
-                    let internal = keyboard_handle.arc.internal.lock().unwrap();
-                    if let Some(focus) = internal.focus.as_ref().and_then(|f| f.0.wl_surface()) {
+                    if let Some(focus) = focus {
                         focus.modifiers(&data.seat, user_data, state.mods, SERIAL_COUNTER.next_serial());
                     }
                 }