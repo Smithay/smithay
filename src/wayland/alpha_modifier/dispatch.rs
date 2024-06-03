1696a99533d113fe1e2367884fadc02d321a99d9
Bartłomiej Maryńczak <marynczak.bartlomiej@gmail.com>
2024-06-03 18:41:07 +0200

    Implement wp alpha modifier v1 (#1426)
    wayland.alpha_modifier: Implement v1 of alpha_modifier protocols=

---
+++ src/wayland/alpha_modifier/dispatch.rs
@@ -0,0 +1,150 @@
+use wayland_protocols::wp::alpha_modifier::v1::server::{
+    wp_alpha_modifier_surface_v1::{self, WpAlphaModifierSurfaceV1},
+    wp_alpha_modifier_v1::{self, WpAlphaModifierV1},
+};
+
+use wayland_server::{
+    backend::ClientId, Client, DataInit, Dispatch, DisplayHandle, GlobalDispatch, New, Resource,
+};
+
+use super::{
+    AlphaModifierState, AlphaModifierSurfaceCachedState, AlphaModifierSurfaceData,
+    AlphaModifierSurfaceUserData,
+};
+use crate::wayland::compositor;
+
+impl<D> GlobalDispatch<WpAlphaModifierV1, (), D> for AlphaModifierState
+where
+    D: GlobalDispatch<WpAlphaModifierV1, ()>,
+    D: Dispatch<WpAlphaModifierV1, ()>,
+    D: Dispatch<WpAlphaModifierSurfaceV1, AlphaModifierSurfaceUserData>,
+    D: 'static,
+{
+    fn bind(
+        _state: &mut D,
+        _: &DisplayHandle,
+        _: &Client,
+        resource: New<WpAlphaModifierV1>,
+        _: &(),
+        data_init: &mut DataInit<'_, D>,
+    ) {
+        data_init.init(resource, ());
+    }
+}
+
+impl<D> Dispatch<WpAlphaModifierV1, (), D> for AlphaModifierState
+where
+    D: Dispatch<WpAlphaModifierV1, ()>,
+    D: Dispatch<WpAlphaModifierSurfaceV1, AlphaModifierSurfaceUserData>,
+    D: 'static,
+{
+    fn request(
+        _state: &mut D,
+        _: &Client,
+        manager: &WpAlphaModifierV1,
+        request: wp_alpha_modifier_v1::Request,
+        _data: &(),
+        _dh: &DisplayHandle,
+        data_init: &mut DataInit<'_, D>,
+    ) {
+        match request {
+            wp_alpha_modifier_v1::Request::GetSurface { id, surface } => {
+                let already_taken = compositor::with_states(&surface, |states| {
+                    states
+                        .data_map
+                        .insert_if_missing_threadsafe(AlphaModifierSurfaceData::new);
+                    let data = states.data_map.get::<AlphaModifierSurfaceData>().unwrap();
+
+                    let already_taken = data.is_resource_attached();
+
+                    if !already_taken {
+                        data.set_is_resource_attached(true);
+                    }
+
+                    already_taken
+                });
+
+                if already_taken {
+                    manager.post_error(
+                        wp_alpha_modifier_v1::Error::AlreadyConstructed,
+                        "wl_surface already has a alpha modifier object attached",
+                    )
+                } else {
+                    data_init.init(id, AlphaModifierSurfaceUserData::new(surface));
+                }
+            }
+
+            wp_alpha_modifier_v1::Request::Destroy => {}
+            _ => unreachable!(),
+        }
+    }
+}
+
+impl<D> Dispatch<WpAlphaModifierSurfaceV1, AlphaModifierSurfaceUserData, D> for AlphaModifierState
+where
+    D: Dispatch<WpAlphaModifierSurfaceV1, AlphaModifierSurfaceUserData>,
+{
+    fn request(
+        _state: &mut D,
+        _: &Client,
+        obj: &WpAlphaModifierSurfaceV1,
+        request: wp_alpha_modifier_surface_v1::Request,
+        data: &AlphaModifierSurfaceUserData,
+        _dh: &DisplayHandle,
+        _: &mut DataInit<'_, D>,
+    ) {
+        match request {
+            wp_alpha_modifier_surface_v1::Request::SetMultiplier { factor } => {
+                let Some(surface) = data.wl_surface() else {
+                    obj.post_error(
+                        wp_alpha_modifier_surface_v1::Error::NoSurface,
+                        "wl_surface was destroyed",
+                    );
+                    return;
+                };
+
+                compositor::with_states(&surface, |states| {
+                    states
+                        .cached_state
+                        .pending::<AlphaModifierSurfaceCachedState>()
+                        .multiplier = Some(factor);
+                })
+            }
+            // Switch back to not specifying the alpha multiplier of this surface.
+            wp_alpha_modifier_surface_v1::Request::Destroy => {
+                let Some(surface) = data.wl_surface() else {
+                    obj.post_error(
+                        wp_alpha_modifier_surface_v1::Error::NoSurface,
+                        "wl_surface was destroyed",
+                    );
+                    return;
+                };
+
+                compositor::with_states(&surface, |states| {
+                    states
+                        .data_map
+                        .get::<AlphaModifierSurfaceData>()
+                        .unwrap()
+                        .set_is_resource_attached(false);
+
+                    states
+                        .cached_state
+                        .pending::<AlphaModifierSurfaceCachedState>()
+                        .multiplier = None;
+                });
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    fn destroyed(
+        _state: &mut D,
+        _client: ClientId,
+        _object: &WpAlphaModifierSurfaceV1,
+        _data: &AlphaModifierSurfaceUserData,
+    ) {
+        // Nothing to do here, graceful Destroy is already handled with double buffering
+        // and in case of client close WlSurface destroyed handler will clean up the data anyway,
+        // so there is no point in queuing new update
+    }
+}
---
+++ src/wayland/alpha_modifier/mod.rs
@@ -0,0 +1,192 @@
+//! Implementation of `wp_alpha_modifier` protocol
+//!
+//! ### Example
+//!
+//! ```no_run
+//! # extern crate wayland_server;
+//! #
+//! use wayland_server::{protocol::wl_surface::WlSurface, DisplayHandle};
+//! use smithay::{
+//!     delegate_alpha_modifier, delegate_compositor,
+//!     wayland::compositor::{self, CompositorState, CompositorClientState, CompositorHandler},
+//!     wayland::alpha_modifier::{AlphaModifierSurfaceCachedState, AlphaModifierState},
+//! };
+//!
+//! pub struct State {
+//!     compositor_state: CompositorState,
+//! };
+//! struct ClientState { compositor_state: CompositorClientState }
+//! impl wayland_server::backend::ClientData for ClientState {}
+//!
+//! delegate_alpha_modifier!(State);
+//! delegate_compositor!(State);
+//!
+//! impl CompositorHandler for State {
+//!    fn compositor_state(&mut self) -> &mut CompositorState {
+//!        &mut self.compositor_state
+//!    }
+//!
+//!    fn client_compositor_state<'a>(&self, client: &'a wayland_server::Client) -> &'a CompositorClientState {
+//!        &client.get_data::<ClientState>().unwrap().compositor_state    
+//!    }
+//!
+//!    fn commit(&mut self, surface: &WlSurface) {
+//!        compositor::with_states(&surface, |states| {
+//!            let current = states.cached_state.current::<AlphaModifierSurfaceCachedState>();
+//!            dbg!(current.multiplier());
+//!        });
+//!    }
+//! }
+//!
+//! let mut display = wayland_server::Display::<State>::new().unwrap();
+//!
+//! let compositor_state = CompositorState::new::<State>(&display.handle());
+//! AlphaModifierState::new::<State>(&display.handle());
+//!
+//! let state = State {
+//!     compositor_state,
+//! };
+//! ```
+
+use std::sync::{
+    atomic::{self, AtomicBool},
+    Mutex,
+};
+
+use wayland_protocols::wp::alpha_modifier::v1::server::{
+    wp_alpha_modifier_surface_v1::WpAlphaModifierSurfaceV1, wp_alpha_modifier_v1::WpAlphaModifierV1,
+};
+use wayland_server::{
+    backend::GlobalId, protocol::wl_surface::WlSurface, Dispatch, DisplayHandle, GlobalDispatch, Resource,
+    Weak,
+};
+
+use super::compositor::Cacheable;
+
+mod dispatch;
+
+/// Data associated with WlSurface
+/// Represents the client pending state
+///
+/// ```no_run
+/// use smithay::wayland::compositor;
+/// use smithay::wayland::alpha_modifier::AlphaModifierSurfaceCachedState;
+///
+/// # let wl_surface = todo!();
+/// compositor::with_states(&wl_surface, |states| {
+///     let current = states.cached_state.current::<AlphaModifierSurfaceCachedState>();
+///     dbg!(current.multiplier());
+/// });
+/// ```
+#[derive(Debug, Clone, Copy, Default)]
+pub struct AlphaModifierSurfaceCachedState {
+    multiplier: Option<u32>,
+}
+
+impl AlphaModifierSurfaceCachedState {
+    /// Alpha multiplier for the surface
+    pub fn multiplier(&self) -> Option<u32> {
+        self.multiplier
+    }
+}
+
+impl Cacheable for AlphaModifierSurfaceCachedState {
+    fn commit(&mut self, _dh: &DisplayHandle) -> Self {
+        *self
+    }
+
+    fn merge_into(self, into: &mut Self, _dh: &DisplayHandle) {
+        *into = self;
+    }
+}
+
+#[derive(Debug)]
+struct AlphaModifierSurfaceData {
+    is_resource_attached: AtomicBool,
+}
+
+impl AlphaModifierSurfaceData {
+    fn new() -> Self {
+        Self {
+            is_resource_attached: AtomicBool::new(false),
+        }
+    }
+
+    fn set_is_resource_attached(&self, is_attached: bool) {
+        self.is_resource_attached
+            .store(is_attached, atomic::Ordering::Release)
+    }
+
+    fn is_resource_attached(&self) -> bool {
+        self.is_resource_attached.load(atomic::Ordering::Acquire)
+    }
+}
+
+/// User data of [WpAlphaModifierSurfaceV1] object
+#[derive(Debug)]
+pub struct AlphaModifierSurfaceUserData(Mutex<Weak<WlSurface>>);
+
+impl AlphaModifierSurfaceUserData {
+    fn new(surface: WlSurface) -> Self {
+        Self(Mutex::new(surface.downgrade()))
+    }
+
+    fn wl_surface(&self) -> Option<WlSurface> {
+        self.0.lock().unwrap().upgrade().ok()
+    }
+}
+
+/// Delegate type for [WpAlphaModifierV1] global.
+#[derive(Debug)]
+pub struct AlphaModifierState {
+    global: GlobalId,
+}
+
+impl AlphaModifierState {
+    /// Regiseter new [WpAlphaModifierV1] global
+    pub fn new<D>(display: &DisplayHandle) -> AlphaModifierState
+    where
+        D: GlobalDispatch<WpAlphaModifierV1, ()>
+            + Dispatch<WpAlphaModifierV1, ()>
+            + Dispatch<WpAlphaModifierSurfaceV1, AlphaModifierSurfaceUserData>
+            + 'static,
+    {
+        let global = display.create_global::<D, WpAlphaModifierV1, _>(1, ());
+
+        AlphaModifierState { global }
+    }
+
+    /// Returns the [WpAlphaModifierV1] global id
+    pub fn global(&self) -> GlobalId {
+        self.global.clone()
+    }
+}
+
+/// Macro to delegate implementation of the alpha modifier protocol
+#[macro_export]
+macro_rules! delegate_alpha_modifier {
+    ($(@<$( $lt:tt $( : $clt:tt $(+ $dlt:tt )* )? ),+>)? $ty: ty) => {
+        type __WpAlphaModifierV1 =
+            $crate::reexports::wayland_protocols::wp::alpha_modifier::v1::server::wp_alpha_modifier_v1::WpAlphaModifierV1;
+        type __WpAlphaModifierSurfaceV1 =
+            $crate::reexports::wayland_protocols::wp::alpha_modifier::v1::server::wp_alpha_modifier_surface_v1::WpAlphaModifierSurfaceV1;
+
+        $crate::reexports::wayland_server::delegate_global_dispatch!($(@< $( $lt $( : $clt $(+ $dlt )* )? ),+ >)? $ty:
+            [
+                __WpAlphaModifierV1: ()
+            ] => $crate::wayland::alpha_modifier::AlphaModifierState
+        );
+
+        $crate::reexports::wayland_server::delegate_dispatch!($(@< $( $lt $( : $clt $(+ $dlt )* )? ),+ >)? $ty:
+            [
+                __WpAlphaModifierV1: ()
+            ] => $crate::wayland::alpha_modifier::AlphaModifierState
+        );
+
+        $crate::reexports::wayland_server::delegate_dispatch!($(@< $( $lt $( : $clt $(+ $dlt )* )? ),+ >)? $ty:
+            [
+                __WpAlphaModifierSurfaceV1: $crate::wayland::alpha_modifier::AlphaModifierSurfaceUserData
+            ] => $crate::wayland::alpha_modifier::AlphaModifierState
+        );
+    };
+}
