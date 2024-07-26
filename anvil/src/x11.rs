8f0e9ae28ce3cdde7a5f3589a13ddd4dccca8fe1
Christian Meissl <meissl.christian@gmail.com>
2024-07-26 19:41:14 +0200

    anvil:  use ImageReader from image crate...
    ...instead of deprecated image::io::Reader

--- anvil/src/x11.rs
+++ anvil/src/x11.rs
@@ -205,7 +205,7 @@ pub fn run_x11() {
 
     #[cfg(feature = "debug")]
     let fps_image =
-        image::io::Reader::with_format(std::io::Cursor::new(FPS_NUMBERS_PNG), image::ImageFormat::Png)
+        image::ImageReader::with_format(std::io::Cursor::new(FPS_NUMBERS_PNG), image::ImageFormat::Png)
             .decode()
             .unwrap();
     #[cfg(feature = "debug")]
