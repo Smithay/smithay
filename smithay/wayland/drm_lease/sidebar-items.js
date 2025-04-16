b8bc76da3229880a20b11cc4feb9ff316cc70b8d
cmeissl <cmeissl@users.noreply.github.com>
2025-04-16 19:41:40 +0000

    deploy: 16b041617ee951f9797a0b12bb0b0f15de0a1b2f

---
+++ smithay/wayland/drm_lease/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["Error"],"struct":["DrmLease","DrmLeaseBuilder","DrmLeaseData","DrmLeaseDeviceGlobalData","DrmLeaseRequest","DrmLeaseRequestData","DrmLeaseState","LeaseRejected"],"trait":["DrmLeaseHandler"]};
---
+++ wayland_protocols/wp/primary_selection/zv1/server/zwp_primary_selection_device_manager_v1/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"constant":["REQ_CREATE_SOURCE_OPCODE","REQ_CREATE_SOURCE_SINCE","REQ_DESTROY_OPCODE","REQ_DESTROY_SINCE","REQ_GET_DEVICE_OPCODE","REQ_GET_DEVICE_SINCE"],"enum":["Event","Request"],"struct":["ZwpPrimarySelectionDeviceManagerV1"]};
