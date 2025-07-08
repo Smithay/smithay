e9ef6c933e322c38b2748f39903f2a42c22b549b
Drakulix <Drakulix@users.noreply.github.com>
2025-07-08 21:05:41 +0000

    deploy: 34feb6bf280bb91027cdb69376331824de5539a3

---
+++ smithay/wayland/shm/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["BufferAccessError"],"fn":["fourcc_to_shm_format","shm_format_to_fourcc","with_buffer_contents","with_buffer_contents_mut","wl_bytes_per_pixel"],"struct":["BufferData","ShmBufferUserData","ShmPoolUserData","ShmState"],"trait":["ShmHandler"]};
---
+++ wayland_protocols/wp/tearing_control/v1/client/wp_tearing_control_manager_v1/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"constant":["REQ_DESTROY_OPCODE","REQ_DESTROY_SINCE","REQ_GET_TEARING_CONTROL_OPCODE","REQ_GET_TEARING_CONTROL_SINCE"],"enum":["Error","Event","Request"],"struct":["WpTearingControlManagerV1"]};
