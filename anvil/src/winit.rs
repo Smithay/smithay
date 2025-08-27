170a000485acb71b84f04985c95c6c59765f7e24
Ian Douglas Scott <idscott@system76.com>
2025-08-27 10:29:04 -0700

    anvil/winit: Handle `CloseRequested`

--- anvil/src/winit.rs
+++ anvil/src/winit.rs
@@ -234,6 +234,7 @@ pub fn run_winit() {
                 crate::shell::fixup_positions(&mut state.space, state.pointer.current_location());
             }
             WinitEvent::Input(event) => state.process_input_event_windowed(event, OUTPUT_NAME),
+            WinitEvent::CloseRequested => state.running.store(false, Ordering::SeqCst),
             _ => (),
         });
 
