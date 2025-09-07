3b516122a593bf41b75b7cde85a80ca7eeff0ff1
Ivan Molodetskikh <yalterz@gmail.com>
2025-09-07 14:01:36 +0300

    Bump WLCS to v1.8.1
    Contains fixes required for enforcing ack before commit as mandated by the
    protocols.

--- compile_wlcs.sh
+++ compile_wlcs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-WLCS_SHA=12234affdc0a4cc104fbaf8a502efc5f822b973b
+WLCS_SHA=2b385328366de5db19521b9b3eef81861430d984
 
 if [ -f "./wlcs/wlcs" ] && [ "$(cd wlcs; git rev-parse HEAD)" = "${WLCS_SHA}" ] ; then
     echo "Using cached WLCS."
