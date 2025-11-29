1b39fe78479e40e545fc0d24b1267d5ca686d806
Erin <erin@erindesu.cz>
2025-11-29 12:45:20 +0100

    chore: extract AtomicFScale into separate module

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -10,9 +10,10 @@ pub(crate) mod ids;
 pub mod user_data;
 
 pub(crate) mod alive_tracker;
-use std::sync::atomic::Ordering;
-
 pub use self::alive_tracker::IsAlive;
+
+mod atomic_scale;
+pub(crate) use atomic_scale::AtomicFScale;
 
 #[cfg(feature = "wayland_frontend")]
 pub(crate) mod iter;
@@ -63,58 +64,3 @@ impl std::fmt::Display for DeadResource {
 }
 
 impl std::error::Error for DeadResource {}
-
-#[cfg(target_has_atomic = "64")]
-type AtomicFScaleValue = f64;
-#[cfg(target_has_atomic = "64")]
-type AtomicFScaleInner = atomic_float::AtomicF64;
-
-#[cfg(not(target_has_atomic = "64"))]
-type AtomicFScaleValue = f32;
-
-#[cfg(not(target_has_atomic = "64"))]
-type AtomicFScaleInner = atomic_float::AtomicF32;
-
-/// Wrapper about either [`AtomicF64`](atomic_float::AtomicF64) or [`AtomicF32`](atomic_float::AtomicF32)
-///
-/// On platforms supporting 64-bit atomics, this acts exactly like
-/// [`AtomicF64`](atomic_float::AtomicF64) would.
-/// 
-/// On platforms having only 32-bit ones, this still has API with [`f64`],
-/// but uses [`AtomicF32`](atomic_float::AtomicF32) internally and
-/// storing operations will lead to precision loss.
-#[derive(Debug)]
-pub struct AtomicFScale(AtomicFScaleInner);
-impl AtomicFScale {
-    /// Loads a value from the atomic float.
-    ///
-    /// On platforms lacking 64-bit atomic support, this will widen the float.
-    #[inline]
-    pub fn load(&self, ordering: Ordering) -> f64 {
-        self.0.load(ordering).into()
-    }
-
-    /// Initialize atomic from value
-    ///
-    /// On platforms lacking 64-bit atomic supports, this means precision loss.
-    #[inline]
-    pub fn new(val: f64) -> Self {
-        Self((val as AtomicFScaleValue).into())
-    }
-
-    /// Stores a value into the atomic float, returning the previous value.
-    ///
-    /// On platforms lacking 64-bit atomic supports, this means precision loss.
-    #[inline]
-    pub fn swap(&self, new_value: f64, ordering: Ordering) -> f64 {
-        self.0.swap(new_value as AtomicFScaleValue, ordering).into()
-    }
-
-    /// Store a value into the atomic float.
-    ///
-    /// On platforms lacking 64-bit atomic supports, this means precision loss.
-    #[inline]
-    pub fn store(&self, new_value: f64, ordering: Ordering) {
-        self.0.store(new_value as AtomicFScaleValue, ordering);
-    }
-}
