86bf2e683ce7d965e9cb81161a9a8671b7214183
Christian Meissl <meissl.christian@gmail.com>
2026-06-11 09:55:35 +0200

    Revert "drm/surface: Make sure we also reset `pending`"
    This break the state after a VT switch as it initializes
    the pending state to the current state, making commit_pending
    always return false and also looses the configured mode.
    
    This reverts commit 13f07dd1f1f20ef0b3a0daa1f860d87f27a816d7.

--- src/backend/drm/surface/atomic.rs
+++ src/backend/drm/surface/atomic.rs
@@ -994,7 +994,6 @@ impl AtomicDrmSurface {
         if res.is_ok() {
             self.used_planes.lock().unwrap().clear();
             self.state.write().unwrap().clear();
-            self.pending.write().unwrap().clear();
         }
 
         res
@@ -1009,7 +1008,6 @@ impl AtomicDrmSurface {
         } else {
             State::current_state(&*self.fd, self.crtc, &mut self.prop_mapping.write().unwrap())?
         };
-        *self.pending.write().unwrap() = self.state.read().unwrap().clone();
         Ok(())
     }
 
